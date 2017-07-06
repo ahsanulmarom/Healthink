194e2130ecba450b782457d71f584901d30ccd88
GamalIS15 <gamaltn@gmail.com>
2017-07-06 10:31:05 +0700

    ~ home masi error

--- app/src/main/java/com/healthink/user/healthink/Into.java
+++ app/src/main/java/com/healthink/user/healthink/Into.java
@@ -2,12 +2,12 @@ package com.healthink.user.healthink;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 
-public class Into extends AppCompatActivity {
+public class Into extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
