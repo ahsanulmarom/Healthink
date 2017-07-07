824824e462277ffdb3d2edb637578e39828b1746
ahsanulmarom <ahsanulmarom@gmail.com>
2017-07-07 21:06:48 +0700

    read data 0.51

---
+++ app/src/main/java/com/healthink/user/healthink/Splash.java
@@ -0,0 +1,26 @@
+package com.healthink.user.healthink;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, Into.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
