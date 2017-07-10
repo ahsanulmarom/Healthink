6069cf78ce6b3f006eef910d235b56cace89850c
ahsanulmarom <ahsanulmarom@gmail.com>
2017-07-10 23:29:13 +0700

    reset password 0.5

--- app/src/main/java/com/healthink/user/healthink/Login.java
+++ app/src/main/java/com/healthink/user/healthink/Login.java
@@ -1,13 +1,19 @@
 package com.healthink.user.healthink;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -19,10 +25,12 @@ import com.google.firebase.auth.FirebaseUser;
 public class Login extends AppCompatActivity {
 
     private EditText email, password;
-    private Button submit, daftar;
+    private Button submit, daftar, reset;
     private FirebaseAuth fAuth;
     private FirebaseAuth.AuthStateListener fStateListener;
     private static final String TAG = SignUp.class.getSimpleName();
+    private Context context = this;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +56,7 @@ public class Login extends AppCompatActivity {
         password = (EditText) findViewById(R.id.login_password);
         submit = (Button) findViewById(R.id.login_btnLogin);
         daftar = (Button) findViewById(R.id.login_btnSignUp);
+        reset = (Button) findViewById(R.id.login_reset);
 
         submit.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -66,6 +75,54 @@ public class Login extends AppCompatActivity {
             public void onClick(View v) {
                 startActivity(new Intent(Login.this, SignUp.class));
                 finish();
+            }
+        });
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                LinearLayout layoutinput = new LinearLayout(context);   //layout
+                layoutinput.setOrientation(LinearLayout.VERTICAL);
+                layoutinput.setPadding(50,50,50,50);
+
+                final TextView txtv = new TextView(context);
+                txtv.setText("Email");
+                txtv.setTextColor(Color.BLACK);
+                layoutinput.addView(txtv);
+
+                final EditText mail = new EditText(context);
+                mail.setInputType(1);
+                mail.setHint("Enter Your EMAIL");
+                layoutinput.addView(mail);
+
+                final TextView txtv2 = new TextView(context);
+                txtv.setText("Username");
+                txtv.setTextColor(Color.BLACK);
+                layoutinput.addView(txtv2);
+
+                final EditText uname = new EditText(context);
+                uname.setInputType(2);
+                uname.setHint("Enter Your USERNAME");
+                layoutinput.addView(uname);
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setTitle("Reset Password");
+                builder.setMessage("Please enter your active email address and your username below." +
+                        "We will send you an email with instruction for resetting your password.");
+                builder.setView(layoutinput);
+                //negative button
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+                //posstive button
+                builder.setPositiveButton("Reset", new  DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        sendEmailResetPassword(mail.getText().toString().trim());
+                    }
+                });
+                builder.show();
             }
         });
     }
@@ -102,6 +159,22 @@ public class Login extends AppCompatActivity {
                     Toast.LENGTH_SHORT).show();
             FirebaseAuth.getInstance().signOut();
         }
+    }
+
+    private void sendEmailResetPassword(String emailAd) {
+        fAuth = FirebaseAuth.getInstance();
+        fAuth.sendPasswordResetEmail(emailAd).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(Login.this, "The instruction has been sent to your email. Please check your email!",
+                            Toast.LENGTH_SHORT).show();
+                } else if(!task.isSuccessful()) {
+                    Toast.makeText(Login.this, "Request failed. Pelase, try again!",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     @Override
