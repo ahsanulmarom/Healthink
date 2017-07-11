727216e403068d63eb3ba7fe2af4098e4d4bb8b2
ahsanulmarom <ahsanulmarom@gmail.com>
2017-07-11 21:34:44 +0700

    drawable2

--- app/src/main/java/com/healthink/user/healthink/SignUp.java
+++ app/src/main/java/com/healthink/user/healthink/SignUp.java
@@ -85,7 +85,6 @@ public class SignUp extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
-
                         // If sign in fails, display a message to the user. If sign in succeeds
                         // the auth state listener will be notified and logic to handle the
                         // signed in user can be handled in the listener.
